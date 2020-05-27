008f108c019b2af854f3bdde0d61f25d3066b681
mushding <ajy1005464@gmail.com>
2020-05-27 10:50:45 +0800

    Updates

---
+++ precache-manifest.b6b36990960b78e1abe9bcf99dbcd065.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9423211298e4fa6a60379b37428d9d8",
+    "url": "/carbon-dioxide-webpage/index.html"
+  },
+  {
+    "revision": "36fabad0cadb4dd39faf",
+    "url": "/carbon-dioxide-webpage/static/css/main.7f76401f.chunk.css"
+  },
+  {
+    "revision": "1c299f054912a1dd01b8",
+    "url": "/carbon-dioxide-webpage/static/js/2.b97dabdd.chunk.js"
+  },
+  {
+    "revision": "ffeb83e48480a4169f7064e81d5a7171",
+    "url": "/carbon-dioxide-webpage/static/js/2.b97dabdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36fabad0cadb4dd39faf",
+    "url": "/carbon-dioxide-webpage/static/js/main.4b98ffb8.chunk.js"
+  },
+  {
+    "revision": "014ad9df6fc4b9a14b9f",
+    "url": "/carbon-dioxide-webpage/static/js/runtime-main.aa3e38b8.js"
+  },
+  {
+    "revision": "336ebad2792632505e3b0037adeeeba2",
+    "url": "/carbon-dioxide-webpage/static/media/bg.336ebad2.png"
+  },
+  {
+    "revision": "e9ac35c6bbbe38b480fcce7f72d26c9d",
+    "url": "/carbon-dioxide-webpage/static/media/jf-openhuninn-1.1.e9ac35c6.ttf"
+  }
+]);
