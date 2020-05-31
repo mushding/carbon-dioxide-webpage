79868577c0646e06756f4e5609f64a90b13405e9
mushding <ajy1005464@gmail.com>
2020-05-31 11:53:07 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/carbon-dioxide-webpage/precache-manifest.3878468e5f30f6e03a8eb5cf74b554fc.js"
+  "/carbon-dioxide-webpage/precache-manifest.01ed2d011680feb512df6293a7710b04.js"
 );
 
 self.addEventListener('message', (event) => {
